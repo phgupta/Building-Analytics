4c48cd2f471dba23e62600e54ae2d3b65ce88f43
Ckoshnick <ckoshnick@gmail.com>
2017-07-26 11:36:27 -0700

    updated docstring
    Clarified main docstring, added V0.3 information

--- building-analytics/CSV_Importer.py
+++ building-analytics/CSV_Importer.py
@@ -1,17 +1,22 @@
 """
-drafty draft for LBNL
-
-## this class imports the data from one or multiple .csv
+## this class imports the data from one or multiple .csv files
 ## Initially this will work for building-level meters data
 ## Initially this will work with .csv files, then it will incorporate the Lucid API (or others)
 ## --- Functionality with .csv
-## Input (args): Specify file name and path, specify (via config file?) name mapping to type of meters
-## ?? does this work for a single file or a whole directory ??
-## right now we want date_time in the first column !!!
-## Output (return): data in a dataframe, metadata table
+## Input (args):
+    fileName = Specify file name 
+    folder = and path, specify [(via config file?) name mapping to type of meters]
+    folderAxis = The direction that the dataframes will be combined based on the folder to folder relationship
+    fileAxis = The direction that the dataframes will be combined based on the folder to folder relationship
+         - folderAxis/fileAxis = 'concat' or 'merge'
+         - typically the folders will represent the time dimension and will be concat (default value)
+         - the files will represnt different metering data and will be merged
+    
+    headRow = which rows to skip, can be a list or single value
+    indexCol = which column from the file is the index, all merged dataframes will be merged on the index (dateTime index)
+
+## Output (return): data in a dataframe, metadata table [does not return meta data at the moment]
 ## Note: may want to have a separate class for data + metadata
-
-
 
 
 V0.1 
@@ -20,16 +25,23 @@ V0.1
 V0.2
 - added: cast numeric on columns that are "object"
 
-TO DO:
-
-- add concatenate files 
-
-- remove the main details 
-
 @author Marco Pritoni <marco.pritoni@gmail.com>
 @author Jacob Rodriguez  <jbrodriguez@ucdavis.edu>
 
-last modified: July 24 2017
+V0.3
+- added functionality where multiple folders and files may be specified
+- handles case where not all files are present in all folders, but the program still runs and fills missing data with NaN
+- added folderAxis / fileAxis direction functionalities
+- added functions: _combine, _head_and_index
+
+
+TO DO:
+    - meta data
+    - what if I want to have different headers for different files (currently the header input header = [0,2,3] will skip rows 0,2,3 from all files that are being loaded)
+    - add robust test cases
+    - improve speed
+
+last modified: July 26 2017
 @author Correy Koshnick <ckoshnick@ucdavis.edu>
 """
 
@@ -50,39 +62,34 @@ class csv_importer(object):
                  indexCol=0,
                  convertCol=True # convertCol specifies if user wants data to all be of numeric type or not. Default is convert to numeric type
                 ):
+        '''
+        Add doc string to explain function
+        '''
+        # loads the TS file or multiple files - it also converts timestamps in date_time format
+        # if no info is provided it assumes the first column is the index and the first row is the header
+        
         
         # the data imported is saved in a dataframe
         self.data=pd.DataFrame()
-        self.tempData = pd.DataFrame()
-        
+        self.tempData = pd.DataFrame()        
         
         self.folderAxis = folderAxis.lower()
         self.fileAxis = fileAxis.lower()
         
-        # loads the TS file or multiple files - it also converts timestamps in date_time format
-        # if no info is provided it assumes the first column is the index and the first row is the header
-        
         ### CHanges to make ###
-        
-        
         # done: look through folders first, so check is instance folder -> list first
         # done: if only 1 folder, handle files with fileAxis='m' or fileAxis='c'
-        
         # done: if many folders, but only 1 file handle with folderAxis = 'm' or folderAxis = 'c'
-        
-        
-        # do If many files in many folders, handle with m x n routine (yet to plan or implement)
+        # done: If many files in many folders, handle with m x n routine (yet to plan or implement)
             # m x n routine plan
             # If folderAxis = fileAxis --> Simple. open all, merge or concat
             
-            #if folderAxis = C and fileAxis = M (MOST COMMON CASE!!)
+            #if folderAxis = C and fileAxis = M
                 # Check files input to generate unique list
                 # try open file in each folder, concat them all in tempDF
                 # After finishing tempDF merge to mainDF
                 # repeat for all file names
-                
-            #if FolerAxis = M and FileAxis = C (IGNORE THIS CASE FOR NOW, SHOULD BE RARE!!)
-        
+   
         if isinstance(folder, list): #########  MANY FOLDER CASES ############
             if isinstance(fileName, list): #MANY FOLDER MANY FILE 
                 _fileList = []
@@ -121,41 +128,29 @@ class csv_importer(object):
                     
                     self.data = self._combine(self.data,self.tempData,direction=self.folderAxis)
                     self.tempData = pd.DataFrame()
-                
-                pass
-    
-            else:  #MANY FOLDER 1 FILE CASE
-                print "#MANY FOLDER 1 FILE CASE"
-                
-                # if many folders, but only 1 file handle wifth olderAxis = 'm' or folderAxis = 'c'
-                
+                    
+            else:   ### #MANY FOLDER 1 FILE CASE ####
+                # if many folders, but only 1 file handle wifth olderAxis = 'm' or folderAxis = 'c'            
                 for i, folder_ in enumerate(folder):
                     _headRow,_indexCol = self._head_and_index(headRow,indexCol,i)
-#                    try:
                     newData = self._load_csv(fileName,folder_,_headRow,_indexCol,convertCol)
                     self.tempData = self._combine(self.tempData,newData, direction = self.folderAxis)
-#                    except:
-#                        print "Something went wrong with loading file %s " %file_
                 self.data = self.tempData  
                
         else: ###################### SINGLE FOLDER CASES  #####################
             
-            if isinstance(fileName, list): #### 1 FOLDER MANY FILES CASE  ####
-            
-                print "#1 FOLDER MANY FILES CASE"
+            if isinstance(fileName, list): #### 1 FOLDER MANY FILES CASE  #####
                 for i, file_ in enumerate(fileName):
                     _headRow,_indexCol = self._head_and_index(headRow,indexCol,i)        
-#                    try:
                     newData = self._load_csv(file_,folder,_headRow,_indexCol,convertCol)
                     self.tempData = self._combine(self.tempData,newData, direction = self.fileAxis)
-#                    except:
-#                        print "Something went wrong with loading file %s " %file_
                 self.data = self.tempData
             
             else: #### SINGLE FOLDER SINGLE FILE CASE ####
                 print "#1 FOLDER 1 FILE CASE"
                 self.data=self._load_csv(fileName,folder,headRow,indexCol)
 
+#### End __init__
 ###############################################################################
     def _combine(self,
                  oldData,
@@ -172,8 +167,7 @@ class csv_importer(object):
             if direction == 'merge':
                 return pd.merge(oldData,newData,how='outer',left_index=True,right_index=True,copy=True)
             elif direction == 'concat' or direction.lower == 'concatentate':
-                return pd.concat([oldData,newData],copy=True)
-                
+                return pd.concat([oldData,newData],copy=True)              
         
     def _head_and_index(self,headRow,indexCol,i):
         # to accept different head and index for each file - following the order in the fileName array
@@ -182,17 +176,12 @@ class csv_importer(object):
             _headRow=headRow[i]
         else:
             _headRow=headRow 
-
         if isinstance(indexCol, list): 
             _indexCol=indexCol[i]
         else:
-            _indexCol=indexCol 
-            
+            _indexCol=indexCol           
         return _headRow,_indexCol
                     
-
-        
-####################################################################################################################################
     def _load_csv(self, 
                   fileName,
                   folder,
@@ -236,30 +225,24 @@ class csv_importer(object):
                 for col in data.columns: # Check columns in dataframe to see if they are numeric
                     if(data[col].dtype != np.number): # If particular column is not numeric, then convert to numeric type
                           data[col]=pd.to_numeric(data[col], errors="coerce")
-
-        
         return data
-####################################################################################################################################
-if __name__=='__main__':
-    
+# END functions   
+###############################################################################
+def _test():
     start_time = timeit.default_timer()
-    # code you want to evaluate
-    
     folder=['test2','test3']
-
     fileName=["data1.csv","data3.csv"]
     rows = [0,4]
     cols = 0
-
     p = csv_importer(fileName,folder,headRow=rows,indexCol=cols,folderAxis='merge',fileAxis = 'concat')
-    
-    print(p.data.shape)
+    elapsed = timeit.default_timer() - start_time
+    print p.data.head(10)
+    print p.data.shape
+    print elapsed, ' seconds to run'
 
-#
-#    # rename for modeling - OAT name is hard coded in the code
-#    print p.data.head(10)
-#    
-#    p.data.rename(columns = {'Temp_Avg':'OAT'}, inplace=True)
-#
-#    elapsed = timeit.default_timer() - start_time
-#    print elapsed
+
+
+if __name__=='__main__':
+    _test()
+
+
